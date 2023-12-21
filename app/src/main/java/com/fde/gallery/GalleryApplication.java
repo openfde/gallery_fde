c5333e7c74fdb8e5fd6cfbc975ad8835253ac5f9
许愿 <xudingqiang520@126.com>
2023-12-21 19:21:26 +0800

    init version

---
+++ app/src/main/java/com/fde/gallery/GalleryApplication.java
@@ -0,0 +1,10 @@
+package com.fde.gallery;
+
+import android.app.Application;
+
+public class GalleryApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
---
+++ app/src/main/java/com/fde/gallery/base/BaseActivity.java
@@ -0,0 +1,6 @@
+package com.fde.gallery.base;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+}
