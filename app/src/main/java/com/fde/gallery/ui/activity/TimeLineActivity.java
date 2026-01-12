cb45ef9455a43960753512ac97b349dc0ea5340c
xudq <xudingqiang520@126.com>
2026-01-12 11:25:33 +0800

    fix build bug

--- app/src/main/java/com/fde/gallery/ui/activity/TimeLineActivity.java
+++ app/src/main/java/com/fde/gallery/ui/activity/TimeLineActivity.java
@@ -2,7 +2,6 @@ package com.fde.gallery.ui.activity;
 
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
-import android.app.ComponentCaller;
 import android.app.RecoverableSecurityException;
 import android.content.Context;
 import android.content.DialogInterface;
