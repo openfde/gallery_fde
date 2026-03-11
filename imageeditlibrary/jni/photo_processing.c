0bd8c2740a42c1d6722cf61148e758f0c6f5201c
xudingqiang <xudingqiang520@126.com>
2026-03-11 14:47:43 +0800

    Added mouse scroll zoom in/out, page flip with left click, added 90-degree rotation, and optimized the operation bar

--- imageeditlibrary/jni/photo_processing.c
+++ imageeditlibrary/jni/photo_processing.c
@@ -35,7 +35,7 @@ void *do_mosaic(void *pix, void *out_pix, unsigned int width, unsigned int heigh
 
 
 JNIEXPORT jint JNICALL
-Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeInitBitmap(
+Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeInitBitmap(
         JNIEnv* env,
         jclass clazz,
         jint width,
@@ -46,35 +46,35 @@ Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeInitBitmap(
 
 //
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeGetBitmapRow(JNIEnv* env, jobject thiz, jint y, jintArray pixels) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeGetBitmapRow(JNIEnv* env, jobject thiz, jint y, jintArray pixels) {
 	int cpixels[bitmap.width];
 	getBitmapRowAsIntegers(&bitmap, (int)y, &cpixels);
 	(*env)->SetIntArrayRegion(env, pixels, 0, bitmap.width, cpixels);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeSetBitmapRow(JNIEnv* env, jobject thiz, jint y, jintArray pixels) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeSetBitmapRow(JNIEnv* env, jobject thiz, jint y, jintArray pixels) {
 	int cpixels[bitmap.width];
 	(*env)->GetIntArrayRegion(env, pixels, 0, bitmap.width, cpixels);
 	setBitmapRowFromIntegers(&bitmap, (int)y, &cpixels);
 }
 
-int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeGetBitmapWidth(JNIEnv* env, jobject thiz) {
+int Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeGetBitmapWidth(JNIEnv* env, jobject thiz) {
 	return bitmap.width;
 }
 
-int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeGetBitmapHeight(JNIEnv* env, jobject thiz) {
+int Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeGetBitmapHeight(JNIEnv* env, jobject thiz) {
 	return bitmap.height;
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeDeleteBitmap(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeDeleteBitmap(JNIEnv* env, jobject thiz) {
 	deleteBitmap(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeFlipHorizontally(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeFlipHorizontally(JNIEnv* env, jobject thiz) {
 	flipHorizontally(&bitmap, 1, 1, 1);
 }
 
-int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeRotate90(JNIEnv* env, jobject thiz) {
+int Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeRotate90(JNIEnv* env, jobject thiz) {
 	int resultCode = rotate90(&bitmap, 1, 1, 1);
 	if (resultCode != MEMORY_OK) {
 		return resultCode;
@@ -85,55 +85,55 @@ int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeRotate9
 	bitmap.height = bitmap.redHeight;
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeRotate180(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeRotate180(JNIEnv* env, jobject thiz) {
 	rotate180(&bitmap, 1, 1, 1);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyInstafix(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyInstafix(JNIEnv* env, jobject thiz) {
 	applyInstafix(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyAnsel(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyAnsel(JNIEnv* env, jobject thiz) {
 	applyAnselFilter(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyTestino(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyTestino(JNIEnv* env, jobject thiz) {
 	applyTestino(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyXPro(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyXPro(JNIEnv* env, jobject thiz) {
 	applyXPro(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyRetro(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyRetro(JNIEnv* env, jobject thiz) {
 	applyRetro(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyBW(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyBW(JNIEnv* env, jobject thiz) {
 	applyBlackAndWhiteFilter(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplySepia(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplySepia(JNIEnv* env, jobject thiz) {
 	applySepia(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyCyano(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyCyano(JNIEnv* env, jobject thiz) {
 	applyCyano(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyGeorgia(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyGeorgia(JNIEnv* env, jobject thiz) {
 	applyGeorgia(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplySahara(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplySahara(JNIEnv* env, jobject thiz) {
 	applySahara(&bitmap);
 }
 
-void Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyHDR(JNIEnv* env, jobject thiz) {
+void Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeApplyHDR(JNIEnv* env, jobject thiz) {
 	applyHDR(&bitmap);
 }
 
-int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeLoadResizedJpegBitmap(JNIEnv* env, jobject thiz, jbyteArray bytes, jint jpegSize, jint maxPixels) {
+int Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeLoadResizedJpegBitmap(JNIEnv* env, jobject thiz, jbyteArray bytes, jint jpegSize, jint maxPixels) {
 	char* jpegData = (char*) (*env)->GetPrimitiveArrayCritical(env, bytes, NULL);
 
 	if (jpegData == NULL) {
@@ -153,7 +153,7 @@ int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeLoadRes
 	return MEMORY_OK;
 }
 
-int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeResizeBitmap(JNIEnv* env, jobject thiz, jint newWidth, jint newHeight) {
+int Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeResizeBitmap(JNIEnv* env, jobject thiz, jint newWidth, jint newHeight) {
 	unsigned char* newRed;
 	int resultCode = newUnsignedCharArray(newWidth*newHeight, &newRed);
 	if (resultCode != MEMORY_OK) {
@@ -195,12 +195,12 @@ int Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeResizeB
 
 //------------------------ beauty module --------------------------------------
 JNIEXPORT void JNICALL
-Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_freeBeautifyMatrix(JNIEnv *env, jobject obj) {
+Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_freeBeautifyMatrix(JNIEnv *env, jobject obj) {
     freeMatrix();
 }
 
 JNIEXPORT void JNICALL
-Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_handleSmoothAndWhiteSkin(JNIEnv *env, jobject obj, jobject bitmap,
+Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_handleSmoothAndWhiteSkin(JNIEnv *env, jobject obj, jobject bitmap,
                                          jfloat smoothValue,jfloat whiteValue) {
     AndroidBitmapInfo info;
     void *pixels;
@@ -235,7 +235,7 @@ Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_handleSmoothAndWh
 
 
 JNIEXPORT void JNICALL
-Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_handleSmooth(JNIEnv *env, jobject obj, jobject bitmap,
+Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_handleSmooth(JNIEnv *env, jobject obj, jobject bitmap,
                                          jfloat smoothValue) {
     AndroidBitmapInfo info;
     void *pixels;
@@ -266,7 +266,7 @@ Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_handleSmooth(JNIE
 }
 
 JNIEXPORT void JNICALL
-Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_handleWhiteSkin(JNIEnv *env, jobject obj, jobject bitmap,
+Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_handleWhiteSkin(JNIEnv *env, jobject obj, jobject bitmap,
                                             jfloat whiteValue) {
     AndroidBitmapInfo info;
     void *pixels;
@@ -298,7 +298,7 @@ Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_handleWhiteSkin(J
 }
 
 JNIEXPORT void JNICALL
-Java_com_fde_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeMosaic(JNIEnv *env, jclass type, jobject bitmap,
+Java_com_xinlan_imageeditlibrary_editimage_fliter_PhotoProcessing_nativeMosaic(JNIEnv *env, jclass type, jobject bitmap,
                                                        jobject out_bitmap,
                                                        jint radius) {
     AndroidBitmapInfo info;
