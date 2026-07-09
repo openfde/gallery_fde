f020792aa2259b760080c8028925d83f29beb8da
xudingqiang <xudingqiang520@126.com>
2026-07-09 10:37:56 +0800

    Solve the problem of videos not being able to be shared

--- app/src/main/java/com/fde/gallery/ui/activity/TimeLineActivity.java
+++ app/src/main/java/com/fde/gallery/ui/activity/TimeLineActivity.java
@@ -212,7 +212,7 @@ public class TimeLineActivity extends BaseActivity implements View.OnClickListen
                     }
                     Intent intent = new Intent(Intent.ACTION_SEND_MULTIPLE);
                     intent.addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION | Intent.FLAG_GRANT_WRITE_URI_PERMISSION);
-                    intent.setType("image/*"); //set MIME type
+                    intent.setType("*/*"); //set MIME type
 //                    intent.putExtra(Intent.EXTRA_STREAM, imageUris.get(0)); //
                     intent.putParcelableArrayListExtra(Intent.EXTRA_STREAM, imageUris);
                     startActivity(Intent.createChooser(intent, context.getString(R.string.share)));
